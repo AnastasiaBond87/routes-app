cfd37dd0ea7a57ba07c56b3037c0bdae75ac2fc8
krevetka87 <bonanast4sia@yandex.ru>
2023-07-28 19:45:38 +0300

    feat: add custom marker icon

--- src/components/Map/Markers/index.tsx
+++ src/components/Map/Markers/index.tsx
@@ -1,9 +1,17 @@
 import { Marker, Popup } from 'react-leaflet';
 import { useAppSelector } from '../../../store/hooks';
+import MarkerIcon from '../../../assets/icons/marker-blue.svg';
+import L from 'leaflet';
 import Title from 'antd/es/typography/Title';
 
 export default function Markers() {
   const { waypoints } = useAppSelector((store) => store.route);
+
+  const Icon = L.icon({
+    iconUrl: MarkerIcon,
+    iconSize: [50, 50],
+    iconAnchor: [25, 50],
+  });
 
   if (!waypoints) {
     return null;
@@ -12,7 +20,7 @@ export default function Markers() {
   return (
     <>
       {waypoints.map(({ location, hint }, index) => (
-        <Marker position={[location[1], location[0]]} key={hint}>
+        <Marker position={[location[1], location[0]]} key={hint} icon={Icon}>
           <Popup>
             <Title level={5}>{`Point#${index + 1}`}</Title>
           </Popup>
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,15 +1,11 @@
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import L from 'leaflet';
 import Markers from './Markers';
 import { mapURL, attribution } from '../../constants';
 import PolyLine from './PolyLine';
 import SetMap from './SetMap';
 
 export default function Map() {
-  L.Marker.prototype.options.icon = L.icon({ iconUrl: icon });
-
   return (
     <MapContainer center={[59.9386, 30.3141]} zoom={12} scrollWheelZoom>
       <TileLayer attribution={attribution} url={mapURL} />
